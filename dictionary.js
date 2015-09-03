50c254b406d81d8b4480be7573e6dc3fc74bf73c
Alpvax <alpvax@netscape.net>
2015-09-03 15:14:38 +0100

    Added getRuneRef function

--- dictionary.js
+++ dictionary.js
@@ -1,2 +1,24 @@
 var root = new Firebase('https://rau.firebaseio.com/');
 var runes = root.child("runes");
+
+getRuneRef(1, 'letters');
+
+function getRuneRef(index, category)
+{
+    var query = runes.child(category).orderByPriority().endAt(index).limitToLast(1)
+    query.on("value", function(querySnapshot)
+    {
+        if(querySnapshot.numChildren() == 1)
+        {
+            // Data is ordered by increasing height, so we want the first entry
+            querySnapshot.forEach(function(rune)
+            {
+                console.log(rune.key());
+                return runes.child(category).child(rune.key());
+            });
+        }else
+        {
+            console.log("No match");
+        }
+    });
+}
