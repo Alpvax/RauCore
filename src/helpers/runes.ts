7b596cc7d7af815431df920e81464abcda34134c
Trymunx <trymunx@gmail.com>
2019-09-23 22:10:41 +0100

    review feedback

--- src/helpers/runes.ts
+++ src/helpers/runes.ts
@@ -1,5 +1,6 @@
 import { Rune } from "@/types";
 
 export const runesToString = (runes: Rune[]): string => {
-  return runes.reduce((str, rune) => str += String.fromCharCode(rune.codepoint), "");
+  return runes.reduce((str, rune) =>
+    str += (typeof rune === "string") ? rune : String.fromCharCode(rune.codepoint), "");
 };
